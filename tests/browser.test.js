c6b47ec4adebb39e27438dc5533cc0d04acc464a
Shania-a <shanya.ny@hotmail.se>
2026-05-13 12:45:27 +0200

    Lagt till selenium test som failar

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,24 +1,24 @@
 const { Builder, By, until } = require('selenium-webdriver');
 require('geckodriver');
 
-// // Här anger vi var testfilen ska hämtas. De konstiga replaceAll-funktionerna ersätter
-// // mellanslag med URL-säkra '%20' och backslash (\) på Windows med slash (/).
-// const fileUnderTest = 'file://' + __dirname.replaceAll(/ /g, '%20').replaceAll(/\\/g, '/') + '/../dist/index.html';
-// const defaultTimeout = 10000;
-// let driver;
-// jest.setTimeout(1000 * 60 * 5); // 5 minuter
+// Här anger vi var testfilen ska hämtas. De konstiga replaceAll-funktionerna ersätter
+// mellanslag med URL-säkra '%20' och backslash (\) på Windows med slash (/).
+const fileUnderTest = 'file://' + __dirname.replaceAll(/ /g, '%20').replaceAll(/\\/g, '/') + '/../dist/index.html';
+const defaultTimeout = 10000;
+let driver;
+jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
-// // Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
-// beforeAll(async () => {
-// console.log(fileUnderTest);
-//     driver = await new Builder().forBrowser('firefox').build();
-//     await driver.get(fileUnderTest);
-// });
+// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
+beforeAll(async () => {
+console.log(fileUnderTest);
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(fileUnderTest);
+});
 
-// // Allra sist avslutar vi Firefox igen
-// afterAll(async() => {
-//     await driver.quit();
-// }, defaultTimeout);
+// Allra sist avslutar vi Firefox igen
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
 
 // test('The stack should be empty in the beginning', async () => {
 //     let stack = await driver.findElement(By.id('top_of_stack')).getText();
@@ -35,3 +35,21 @@ require('geckodriver');
 //     });
 // });
 
+// Testar sidans titel
+test('Page title should be correct', async () => {
+    await driver.get('file://' + __dirname + '/../dist/index.html');
+    let title = await driver.getTitle();
+    
+    // Detta kommer FAILA för att titeln är "En stack"
+    expect(title).toBe('Helt Fel Titel'); 
+});
+
+// Kollar så att stacken i startläget är n/a
+describe('Initial UI State', () => {
+    it('should show n/a as the initial stack value', async () => {
+        let stackText = await driver.findElement(By.id('top_of_stack')).getText();
+        
+        // Detta kommer FAILA för att texten är "n/a"
+        expect(stackText).toBe('Stacken är full');
+    });
+});
